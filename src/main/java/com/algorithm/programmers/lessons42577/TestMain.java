96b229c5d353194c27a885156bf80f596eade53f
kjkim <apio1130@naver.com>
2019-12-29 17:45:41 +0900

    프로그래머스 lessons 42577 : 전화번호 목록

---
+++ src/main/java/com/algorithm/programmers/lessons42577/TestMain.java
@@ -0,0 +1,11 @@
+package com.algorithm.programmers.lessons42577;
+
+class TestMain {
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+//		String[] phoneBook = { "119", "97674223", "1195524421" };
+		String[] phoneBook = { "123","456","789" };
+		System.out.println(solution.solution(phoneBook));
+	}
+
+}
