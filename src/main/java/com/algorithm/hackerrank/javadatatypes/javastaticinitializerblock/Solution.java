4994aa4e85d877e7a0264c3aa448a05fff641d9a
kjkim <apio1130@naver.com>
2020-02-17 22:53:32 +0900

    hackerrank : Mini-Max Sum

--- src/main/java/com/algorithm/hackerrank/javadatatypes/javastaticinitializerblock/Solution.java
+++ src/main/java/com/algorithm/hackerrank/javadatatypes/javastaticinitializerblock/Solution.java
@@ -8,29 +8,29 @@ import java.util.Scanner;
  */
 public class Solution {
 
-	// Write your code here
-	static int B;
-	static int H;
-	static boolean flag;
-	static Scanner sc;
-	
-	static {
-		sc = new Scanner(System.in);
-		B = sc.nextInt();
-		H = sc.nextInt();
-		if (B > 0 && H > 0) {
-			flag = true;
-		} else {
-			System.out.println("java.lang.Exception: Breadth and height must be positive");
-		}
-	}
+    // Write your code here
+    static int B;
+    static int H;
+    static boolean flag;
+    static Scanner sc;
 
-	public static void main(String[] args) {
-		if (flag) {
-			int area = B * H;
-			System.out.print(area);
-		}
+    static {
+        sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B > 0 && H > 0) {
+            flag = true;
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
 
-	}// end of main
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+
+    }// end of main
 
 }// end of class
