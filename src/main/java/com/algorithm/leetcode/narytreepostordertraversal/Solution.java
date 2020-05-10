7caf9e4b9e8ae57cdb664b13694bda4963bd10fe
kjkim <apio1130@naver.com>
2020-05-10 17:02:48 +0900

    leetcode - N-ary Tree Postorder Traversal

--- src/main/java/com/algorithm/leetcode/narytreepostordertraversal/Solution.java
+++ src/main/java/com/algorithm/leetcode/narytreepostordertraversal/Solution.java
@@ -1,5 +1,50 @@
 package com.algorithm.leetcode.narytreepostordertraversal;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * 590. N-ary Tree Postorder Traversal
+ *
+ */
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        getPostorderInfo(list, root);
+        return list;
+    }
+
+    private void getPostorderInfo(List<Integer> list, Node node) {
+        if (node != null) {
+            List<Node> childs = node.children;
+            if (childs == null || childs.isEmpty()) {
+                list.add(node.val);
+            } else {
+                for (Node n : childs) {
+                    getPostorderInfo(list, n);
+                }
+                list.add(node.val);
+            }
+        }
+    }
+}
+
+/**
+ * Definition for a Node.
+ */
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
 }
