67efb3308da2bbe20fe51dd3115f15faab76fd1f
kjkim <apio1130@naver.com>
2020-05-19 22:02:08 +0900

    leetcode - Reverse Linked List

---
+++ src/main/java/com/algorithm/leetcode/reverselinkedlist/Solution.java
@@ -0,0 +1,52 @@
+package com.algorithm.leetcode.reverselinkedlist;
+
+import java.util.Stack;
+
+/**
+ * 206. Reverse Linked List
+ *
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        Stack<ListNode> stack = new Stack<>();
+        stack.push(head);
+
+        ListNode temp = head.next;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+
+        ListNode reverseNodes = stack.pop();
+        temp = reverseNodes;
+        while (!stack.isEmpty()) {
+            temp.next = stack.pop();
+            temp = temp.next;
+        }
+        temp.next = null;
+
+        return reverseNodes;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ *
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
