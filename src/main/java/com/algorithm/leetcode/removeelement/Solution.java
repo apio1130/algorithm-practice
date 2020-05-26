136061d57f78be5b0540e4420300b1b72f870ca4
kjkim <apio1130@naver.com>
2020-05-27 00:15:56 +0900

    leetcode - Remove Element

--- src/main/java/com/algorithm/leetcode/removeelement/Solution.java
+++ src/main/java/com/algorithm/leetcode/removeelement/Solution.java
@@ -9,9 +9,7 @@ class Solution {
         int idx = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                int temp = nums[idx];
                 nums[idx] = nums[i];
-                nums[i] = temp;
                 idx++;
             }
         }
