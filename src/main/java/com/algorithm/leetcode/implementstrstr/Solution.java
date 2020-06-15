ada79774afeb4c6fa918b683efbbff378bca2b0f
kjkim <apio1130@naver.com>
2020-06-15 22:58:48 +0900

    leetcode - Implement strStr()

---
+++ src/main/java/com/algorithm/leetcode/implementstrstr/Solution.java
@@ -0,0 +1,10 @@
+package com.algorithm.leetcode.implementstrstr;
+
+/**
+ *  28. Implement strStr()
+ */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
