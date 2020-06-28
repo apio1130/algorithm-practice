25d10fd5e309d3fe0d6d53da66594fae620e694b
kjkim <apio1130@naver.com>
2020-06-29 00:00:00 +0900

    leetcode - Reverse Bits

---
+++ src/main/java/com/algorithm/leetcode/reversebits/Solution.java
@@ -0,0 +1,18 @@
+package com.algorithm.leetcode.reversebits;
+
+/**
+ * 190. Reverse Bits
+ *
+ */
+class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        System.out.println(Integer.toBinaryString(n));
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.reverseBits(10);
+    }
+}
