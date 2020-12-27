f0117bb4234f4b244d8f4112ffc787db45c87c4d
apio1130 <apio1130@naver.com>
2020-12-27 21:02:37 +0900

    refactor(baekjoon): problem-10989  수 정렬하기 3

--- src/main/java/com/algorithm/baekjoon/pb10989/Main.java
+++ src/main/java/com/algorithm/baekjoon/pb10989/Main.java
@@ -10,27 +10,33 @@ import java.io.OutputStreamWriter;
  * 수 정렬하기 3
  */
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         final int MAX_SIZE = 10000;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
-        int[] numArr = new int[MAX_SIZE + 1];
-        for (int i = 0; i < size; i++) {
-            int num = Integer.parseInt(br.readLine());
-            numArr[num]++;
+        int[] numArr = new int[0];
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int size = Integer.parseInt(br.readLine());
+            numArr = new int[MAX_SIZE + 1];
+            for (int i = 0; i < size; i++) {
+                int num = Integer.parseInt(br.readLine());
+                numArr[num]++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i = 1; i <= MAX_SIZE; i++) {
-            if (numArr[i] > 0) {
-                int count = numArr[i];
-                String numbTxt = i + "\n";
-                for (int j = 0; j < count; j++) {
-                    bw.write(numbTxt);
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            for (int i = 1; i <= MAX_SIZE; i++) {
+                if (numArr[i] > 0) {
+                    int count = numArr[i];
+                    String numbTxt = i + "\n";
+                    for (int j = 0; j < count; j++) {
+                        bw.write(numbTxt);
+                    }
                 }
             }
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        bw.flush();
     }
 }
