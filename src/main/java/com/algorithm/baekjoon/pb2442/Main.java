40e7ef2ac60dc4478eb7efa91b99589bae22b77e
kjkim <apio1130@naver.com>
2021-01-26 00:06:41 +0900

    feat(baekjoon): problem-2442 별 찍기(5)

--- src/main/java/com/algorithm/baekjoon/pb2442/Main.java
+++ src/main/java/com/algorithm/baekjoon/pb2442/Main.java
@@ -1,4 +1,4 @@
-package com.algorithm.programmers.pb2442;
+package com.algorithm.baekjoon.pb2442;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,16 +8,14 @@ import java.io.InputStreamReader;
  * 별 찍기 - 5
  *
  */
-class Solution {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            sb.append(" ".repeat(n-i))
-              .append("*".repeat(i * 3))
-              .append(System.lineSeparator());
+            sb.append(" ".repeat(n - i)).append("*".repeat(2 * i - 1)).append(System.lineSeparator());
         }
         System.out.println(sb.toString());
     }
