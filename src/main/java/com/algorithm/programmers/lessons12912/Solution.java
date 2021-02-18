45a5b9552ec4c2db1bc34458adf5371e880b2c68
apio1130 <apio1130@naver.com>
2021-02-18 23:54:56 +0900

    feat(programmers): lessons12912 두 정수 사이의 합

--- src/main/java/com/algorithm/programmers/lessons12912/Solution.java
+++ src/main/java/com/algorithm/programmers/lessons12912/Solution.java
@@ -1,14 +1,15 @@
 package com.algorithm.programmers.lessons12912;
 
-import java.util.stream.LongStream;
-
+/**
+ * 두 정수 사이의 합
+ */
 class Solution {
     public long solution(int a, int b) {
         if (a == b) {
             return a;
         }
-        int min = Math.min(a, b);
-        int max = Math.max(a, b);
-        return LongStream.range(min, max + 1).sum();
+        long min = Math.min(a, b);
+        long max = Math.max(a, b);
+        return (long)((min + max) * (max - min + 1)) / 2;
     }
 }
