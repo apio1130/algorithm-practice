829d016faac7879bf66fa1d7a2c515266855b802
apio1130 <apio1130@naver.com>
2021-02-19 23:30:08 +0900

    feat(programmers): lessons12954 x만큼 간격이 있는 n개의 숫자

---
+++ src/main/java/com/algorithm/programmers/lessons12954/Solution.java
@@ -0,0 +1,15 @@
+package com.algorithm.programmers.lessons12954;
+
+/**
+ * x만큼 간격이 있는 n개의 숫자
+ */
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for(int i = 1; i <= n; i++) {
+            answer[i-1] = (long)x * i;
+        }
+
+        return answer;
+    }
+}
