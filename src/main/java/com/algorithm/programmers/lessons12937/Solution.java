0f9865b6c0d658063407c35493f2b7d81d10fb61
apio1130 <apio1130@naver.com>
2021-03-30 00:31:01 +0900

    feat(programmers): lessons12937 짝수와 홀수

--- src/main/java/com/algorithm/programmers/lessons12937/Solution.java
+++ src/main/java/com/algorithm/programmers/lessons12937/Solution.java
@@ -5,9 +5,6 @@ package com.algorithm.programmers.lessons12937;
  */
 class Solution {
     public String solution(int num) {
-        if (num < 0) {
-            num *= -1;
-        }
-        return num % 2 == 1 ? "Odd" : "Even";
+        return num % 2 == 0 ? "Even" : "Odd";
     }
 }
