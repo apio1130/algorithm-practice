368069d3a3024fa8ad2b9e4fb23fb1cfe78bb852
apio1130 <apio1130@naver.com>
2021-05-08 23:58:19 +0900

    feat(baekjoon): problem1267 핸드폰 요금

---
+++ src/main/java/com/algorithm/baekjoon/pb1267/Main.java
@@ -0,0 +1,32 @@
+package com.algorithm.baekjoon.pb1267;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 핸드폰 요금
+ */
+class Main {
+	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int y = 0;
+		int m = 0;
+
+		for (int i = 0; i < n; i++) {
+			double temp = Double.parseDouble(st.nextToken());
+			y += Math.ceil(temp / 30) * 10;
+			m += Math.ceil(temp / 60) * 15;
+		}
+
+		if (y == m) {
+			System.out.printf("Y M %d\n", y);
+		} else if (y < m) {
+			System.out.printf("Y %d\n", y);
+		} else {
+			System.out.printf("M %d\n", m);
+		}
+	}
+}
