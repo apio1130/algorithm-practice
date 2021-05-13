0e47f74f40e7e20b2d4a7460117c3e42cf96e241
apio1130 <apio1130@naver.com>
2021-05-13 23:27:52 +0900

    fix(leetcode): 1704. Determine if String Halves Are Alike

--- src/main/java/com/algorithm/leetcode/determineifstringhalvesarealike/Solution.java
+++ src/main/java/com/algorithm/leetcode/determineifstringhalvesarealike/Solution.java
@@ -21,10 +21,7 @@ class Solution {
 	}
 
 	private boolean checkVowel(char c) {
-		if ('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c
-			|| 'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c) {
-			return true;
-		}
-		return false;
+		return 'a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c
+			|| 'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c;
 	}
 }
