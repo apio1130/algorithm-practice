1ca021c8e5de262a3e5e0b620576d8bcd62d7698
apio1130 <apio1130@naver.com>
2021-05-13 23:26:03 +0900

    feat(leetcode): 1704. Determine if String Halves Are Alike

--- src/main/java/com/algorithm/leetcode/determineifstringhalvesarealike/Solution.java
+++ src/main/java/com/algorithm/leetcode/determineifstringhalvesarealike/Solution.java
@@ -1,33 +1,30 @@
 package com.algorithm.leetcode.determineifstringhalvesarealike;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * 1704. Determine if String Halves Are Alike
  */
 class Solution {
 	public boolean halvesAreAlike(String s) {
-		char[] left = s.substring(0, s.length() / 2).toCharArray();
-		char[] right = s.substring(s.length() / 2).toCharArray();
-
-		List<Character> list = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-		int leftCnt = 0;
-		int rightCnt = 0;
-		for (char c: left) {
-			if(list.indexOf(c) > -1) {
-				leftCnt++;
+		int count = 0;
+		for (int i = 0; i < s.length() / 2; i++) {
+			if(checkVowel(s.charAt(i))) {
+				count++;
 			}
 		}
-		for (char c: right) {
-			if(list.indexOf(c) > -1) {
-				rightCnt++;
+		for (int i = s.length() / 2; i < s.length(); i++) {
+			if(checkVowel(s.charAt(i))) {
+				count--;
 			}
 		}
 
-		System.out.println(left);
-		System.out.println(right);
+		return count == 0;
+	}
 
-		return leftCnt == rightCnt;
+	private boolean checkVowel(char c) {
+		if ('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c
+			|| 'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c) {
+			return true;
+		}
+		return false;
 	}
 }
