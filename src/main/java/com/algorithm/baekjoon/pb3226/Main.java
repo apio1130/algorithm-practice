ceb633277f1565f061df3ac26106ce3ce5f36dbc
apio1130 <apio1130@naver.com>
2021-05-31 01:16:00 +0900

    refactor(baekjoon):problem 3226 전화 요금

--- src/main/java/com/algorithm/baekjoon/pb3226/Main.java
+++ src/main/java/com/algorithm/baekjoon/pb3226/Main.java
@@ -14,33 +14,42 @@ class Main {
 		int sum = 0;
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			String[] time = st.nextToken().split(":");
-			int hour = Integer.parseInt(time[0], 10);
-			int minute = Integer.parseInt(time[1], 10);
+			String[] input = st.nextToken().split(":");
+			int time = Integer.parseInt(input[0], 10) * 60 + Integer.parseInt(input[1], 10);
 			int period = Integer.parseInt(st.nextToken(), 10);
-			int diff = minute + period - 60;
-			sum += getPrice(hour, period, diff);
+			sum += getPrice(time, period);
 		}
 		System.out.println(sum);
 	}
 
-	private static int getPrice(int hour, int period, int diff) {
+	private static int getPrice(int time, int period) {
+		int endTime = time + period;
 		int price = 0;
-		if (hour >= 7 && hour < 19) {
-			if (hour == 18 && diff > 0) {
-				price += (period - diff) * 10;
-				price += diff * 5;
-			} else {
+
+		if (isVatStart(time)) {
+			if (isVatStart(endTime)) {
 				price = period * 10;
+			} else {
+				int diff = endTime % 60;
+				price += diff * 5;
+				price += (period - diff) * 10;
 			}
 		} else {
-			if (hour == 6 && diff > 0) {
-				price += (period - diff) * 5;
+			if (isVatStart(endTime)) {
+				int diff = endTime % 60;
 				price += diff * 10;
+				price += (period - diff) * 5;
 			} else {
 				price = period * 5;
 			}
 		}
+
 		return price;
+	}
+
+	private static boolean isVatStart(int time) {
+		final int VAT_START_TIME = 7 * 60;
+		final int VAT_END_TIME = 19 * 60;
+		return time >= VAT_START_TIME && time < VAT_END_TIME;
 	}
 }
