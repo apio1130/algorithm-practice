b52374113dfb1e11d30f0ef962052b3a8c975764
apio1130 <apio1130@naver.com>
2021-06-01 07:17:55 +0900

    refactor(leetcode): 1365. How Many Numbers Are Smaller Than the Current Number

--- src/main/java/com/algorithm/leetcode/howmanynumbersaresmallerthanthecurrentnumber/Solution.java
+++ src/main/java/com/algorithm/leetcode/howmanynumbersaresmallerthanthecurrentnumber/Solution.java
@@ -1,20 +1,18 @@
 package com.algorithm.leetcode.howmanynumbersaresmallerthanthecurrentnumber;
 
-import java.util.Arrays;
-import java.util.function.Supplier;
-import java.util.stream.IntStream;
-
 /**
  * 1365. How Many Numbers Are Smaller Than the Current Number
  */
 class Solution {
 	public int[] smallerNumbersThanCurrent(int[] nums) {
 		int[] result = new int[nums.length];
-		Supplier<IntStream> supplier = () -> Arrays.stream(nums);
-
 		for (int i = 0; i < nums.length; i++) {
-			int num = nums[i];
-			int count = (int)supplier.get().filter(n -> n < num).count();
+			int count = 0;
+			for (int j = 0; j < nums.length; j++) {
+				if (nums[i] > nums[j]) {
+					count++;
+				}
+			}
 			result[i] = count;
 		}
 		return result;
