cde37aa3eaccf82de4b636a64d3a626e956c4b17
apio1130 <apio1130@naver.com>
2021-06-04 00:22:33 +0900

    refactor(baekjoon): problem2480 주사위 세개

--- src/main/java/com/algorithm/baekjoon/pb2480/Main.java
+++ src/main/java/com/algorithm/baekjoon/pb2480/Main.java
@@ -5,32 +5,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * problem 2480 주사위 세개
+ * problem2480 주사위 세개
  */
 class Main {
 	public static void main(String... args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] nums = new int[3];
+		int[] selectedDice = new int[7];
 		int max = 0;
-		for (int i = 0; i < nums.length; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-			if (nums[i] > max) {
-				max = nums[i];
+		for (int i = 0; i < 3; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			selectedDice[num]++;
+			if (num > max) {
+				max = num;
 			}
 		}
-		int result = getMoney(nums, max);
-		System.out.println(result);
-	}
 
-	private static int getMoney(int[] nums, int max) {
-		if (nums[0] == nums[1] && nums[0] == nums[2]) {
-			return nums[0] * 1000 + 10000;
-		} else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
-			int temp = nums[0] == nums[1] ? nums[0] : nums[2];
-			return 1000 + temp * 100;
-		} else {
-			return max * 100;
+		int result = max * 100;
+		for (int i = 1; i < selectedDice.length; i++) {
+			int count = selectedDice[i];
+			if (count == 3) {
+				result = 10000 + i * 1000;
+				break;
+			} else if (count == 2) {
+				result = 1000 + i * 100;
+				break;
+			}
 		}
+		System.out.println(result);
 	}
 }
