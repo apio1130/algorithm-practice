31475f1c93160cff1384da0dc169a67c62028eb3
apio1130 <apio1130@naver.com>
2021-06-09 00:13:28 +0900

    feat(baekjoon): problem2914 저작권

--- src/main/java/com/algorithm/baekjoon/pb2914/Main.java
+++ src/main/java/com/algorithm/baekjoon/pb2914/Main.java
@@ -1,10 +1,20 @@
 package com.algorithm.baekjoon.pb2914;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * 저작권
  */
 class Main {
 	public static void main(String... args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputs = br.readLine().split(" ");
 
+		int a = Integer.parseInt(inputs[0]);
+		int b = Integer.parseInt(inputs[1]);
+
+		int startCnt = a * (b - 1);
+		System.out.println(startCnt + 1);
 	}
 }
