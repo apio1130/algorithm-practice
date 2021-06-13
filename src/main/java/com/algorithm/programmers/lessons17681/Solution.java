9886deca1304db52f97030b97f771f783e20b044
apio1130 <apio1130@naver.com>
2021-06-13 23:50:27 +0900

    refactor(programmers): lessons17681 비밀지도

--- src/main/java/com/algorithm/programmers/lessons17681/Solution.java
+++ src/main/java/com/algorithm/programmers/lessons17681/Solution.java
@@ -7,12 +7,9 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
-            String temp = Integer.toString(arr1[i] | arr2[i], 2)
+            String temp = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]))
                     .replaceAll("1", "#")
                     .replaceAll("0", " ");
-            if (temp.length() < n) {
-                temp = " ".repeat(n - temp.length()).concat(temp);
-            }
             answer[i] = temp;
         }
         return answer;
