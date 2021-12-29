79b64b5ee2ea67798c6962c8f3f8cddd5975254f
apio1130 <apio1130@naver.com>
2021-12-29 23:56:06 +0900

    feat(leetcode): 338. Counting Bits

---
+++ src/main/java/com/algorithm/leetcode/countingbits/Solution.java
@@ -0,0 +1,14 @@
+package com.algorithm.leetcode.countingbits;
+
+/**
+ * 338. Counting Bits
+ */
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+        for(int i = 0; i <= n; i++) {
+            result[i] = Integer.bitCount(i);
+        }
+        return result;
+    }
+}
