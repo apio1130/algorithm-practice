6e8a01005c120052ce211adcf68078f6fcbc7230
kjkim <apio1130@naver.com>
2022-10-11 00:15:14 +0900

    feat(hackerrank): Java Date and Time

---
+++ src/main/java/com/algorithm/hackerrank/javadateandtime/Result.java
@@ -0,0 +1,40 @@
+package com.algorithm.hackerrank.javadateandtime;
+
+import java.io.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+    public static String findDay(int month, int day, int year) {
+        LocalDate localDate = LocalDate.of(year, month, day);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        return dayOfWeek.getDisplayName(TextStyle.FULL, Locale.US).toUpperCase();
+    }
+
+    public static void main(String... args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+        int day = Integer.parseInt(firstMultipleInput[1]);
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+        System.out.println(res);
+
+        bufferedReader.close();
+    }
+
+}
